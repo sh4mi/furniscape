609b30ebef1f02d8a57f1c82e8c915b00742bd6e
Sumaiya98-lab <108512512+Sumaiya98-lab@users.noreply.github.com>
2024-05-29 04:57:13 +0500

    Cart change

--- resources/views/web/layouts/header.blade.php
+++ resources/views/web/layouts/header.blade.php
@@ -70,49 +70,7 @@
                             <!-- / Cart Header-->
 
                             <!-- Cart Items-->
-                            <div>
-
-                                <!-- Cart Product-->
-                                <div class="row mx-0 py-4 g-0 border-bottom">
-                                    <div class="col-2 position-relative">
-                                        <picture class="d-block ">
-                                            <img class="img-fluid" src="./assets/images/products/product-cart-1.jpg"
-                                                alt="Furniscape">
-                                        </picture>
-                                    </div>
-                                    <div class="col-9 offset-1">
-                                        <div>
-                                            <h6 class="justify-content-between d-flex align-items-start mb-2">
-                                                Nike Air VaporMax 2021
-                                                <i class="ri-close-line ms-3"></i>
-                                            </h6>
-                                            <span class="d-block text-muted fw-bolder text-uppercase fs-9">Size: 9 /
-                                                Qty: 1</span>
-                                        </div>
-                                        <p class="fw-bolder text-end text-muted m-0">$85.00</p>
-                                    </div>
-                                </div>
-                                <!-- Cart Product-->
-                                <div class="row mx-0 py-4 g-0 border-bottom">
-                                    <div class="col-2 position-relative">
-                                        <picture class="d-block ">
-                                            <img class="img-fluid" src="./assets/images/products/product-cart-2.jpg"
-                                                alt="Furniscape">
-                                        </picture>
-                                    </div>
-                                    <div class="col-9 offset-1">
-                                        <div>
-                                            <h6 class="justify-content-between d-flex align-items-start mb-2">
-                                                Nike ZoomX Vaporfly
-                                                <i class="ri-close-line ms-3"></i>
-                                            </h6>
-                                            <span class="d-block text-muted fw-bolder text-uppercase fs-9">Size: 11 /
-                                                Qty: 1</span>
-                                        </div>
-                                        <p class="fw-bolder text-end text-muted m-0">$125.00</p>
-                                    </div>
-                                </div>
-                            </div>
+                           
                             <!-- /Cart Items-->
 
                             <!-- Cart Summary-->
