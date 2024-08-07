d50727c7d861eb7be053453016a1fe01250c753a
shahmir alam <shahmir.byteshifted@gmail.com>
2024-08-07 14:49:59 +0500

    adas

---
+++ resources/views/emails/confirmation.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Mail</title>
+</head>
+
+<body>
+    <h1>Hello, this is my mail content!</h1>
+    <p>This is a sample email sent from Laravel.</p>
+</body>
+
+</html>
